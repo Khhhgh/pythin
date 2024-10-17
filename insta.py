7ea09a37f8a3ce8efceb2fc50965d2bbeb289b39
ammaar2 <ammerrafa@gmail.com>
2024-10-17 09:52:28 +0300

    Update insta.py

---
+++ insta.py
@@ -0,0 +1,51 @@
+import telebot
+import os
+#s1
+#
+#لاتخمط منيوش
+try:
+    from Topython import Instagram
+except ImportError:
+    os.system("pip install Topython")
+try:
+    import socks
+except:
+    os.system("pip install socks")
+from Topython import Instagram
+import random
+from rich import print
+from rich.console import Console
+from rich.progress import track
+
+
+token = input("Enter Token: ")
+chat_id = input("Enter Chat ID: ")
+bot = telebot.TeleBot(token)
+
+
+good = 0
+bad = 0
+
+def s1():
+    global good, bad, bot, chat_id
+    console = Console()
+
+    
+    for _ in track(range(200), description="Checking Instagram users..."):
+        uh = "".join(random.choice("1234567890") for _ in range(1))
+        ns = "".join(random.choice("qwertyuiopasdfghjklzxcvbnm._") for _ in range(1))
+        j = "".join(random.choice("1234567890qwertyuiopasdfghjklzxcvbnm._") for _ in range(1))
+        user = f"__{j}{ns}{uh}"
+
+        check = Instagram.CheckUsers(f"{user}")
+        if check == True:
+            good += 1
+            print(f"{good} - [+] Good user: {user}")
+            bot.send_photo(chat_id, "https://t.me/ismhodi/51", caption=f"Good user: {user}\nBy: @yzzyyzy")
+        else:
+            bad += 1
+            print(f"{bad} - [-] Bad user: {user}")
+
+
+s1()#s1
+bot.polling()
